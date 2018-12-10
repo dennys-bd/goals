b9ed4642112cd7266c611b9b01528b24d06a8c57
Dennys Azevedo <dennys.bd@gmail.com>
2018-12-10 09:52:35 -0300

    Goals Version

---
+++ cmd/version.go
@@ -0,0 +1,12 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var versionCmd = &cobra.Command{
+	Use:     "version",
+	Aliases: []string{"Version", "v"},
+	Short:   "Version says the current version of goals",
+	Run: func(cmd *cobra.Command, args []string) {
+		println("Goals Version: Beta 0.2")
+	},
+}
